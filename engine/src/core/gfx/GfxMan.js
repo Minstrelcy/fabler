5802e5ce9406001d4acf486b716ba2bfedf81b21
J.R. Omahen <composr@gmail.com>
2014-06-22 05:38:16 -0700

    Cleaned up code style running through JSLint.

--- engine/src/core/gfx/GfxMan.js
+++ engine/src/core/gfx/GfxMan.js
@@ -1,39 +1,39 @@
 // GfxMan: The Graphics Manager for Fabler
 // Copyright (C) 2014 J. R. Omahen
 
-var GfxMan = (function (){
+FABLER.GfxMan = (function () {
+    "use strict";
+
     // Attributes
-    var _canvas;
-    var _canvasId = 'mainBuffer';
-    var _gfxContext;
+    var canvas, gfxContext, canvasId = 'mainBuffer';
 
     // Private Methods
-    function _createCanvas(height, width) {
-	if (typeof _canvas === 'undefined') {
-	    var body = document.getElementsByTagName('body')[0];
+    function createCanvas(height, width) {
+        if (canvas === undefined) {
+            var body = document.getElementsByTagName('body')[0];
 
-	    _canvas = document.createElement('canvas');
-	    _canvas.id = _canvasId;
-	    _canvas.height = height;
-	    _canvas.width = width;
+            canvas = document.createElement('canvas');
+            canvas.id = canvasId;
+            canvas.height = height;
+            canvas.width = width;
 
-	    body.appendChild(_canvas);
-	}
-    };
+            body.appendChild(canvas);
+        }
+    }
 
-    function _initGfxContext() {
-	if (typeof _gfxContext === 'undefined') {
-	    _gfxContext = _canvas.getContext('2d');
-	}
-    };
+    function initGfxContext() {
+        if (gfxContext === undefined) {
+            gfxContext = canvas.getContext('2d');
+        }
+    }
 
     // Public Methods
     return {
 
-	init: function () {
-	    _createCanvas(800, 600);
-	    _initGfxContext();
-	}
+        init: function () {
+            createCanvas(800, 600);
+            initGfxContext();
+        }
 
     };
 }());
