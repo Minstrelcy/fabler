629561a6eefb7fd9b8fb3ea7ce82577a943dcc74
J.R. Omahen <composr@gmail.com>
2014-07-02 18:52:17 -0700

    Fixed build for locations

--- engine/src/core/modules/world/locations.js
+++ engine/src/core/modules/world/locations.js
@@ -1,34 +1,37 @@
 /* Idea is to have an array called locations that will contain all the actual world information */
 
 var locations = {
-        alleyEntrance: {
-                name: "Entrance to Diagon Alley",
-                description: "This is the entrance to Diagon Alley.",
-                on_enter: function() {
-                        console.log("You are now entering Diagon Alley.");
-                },
-                objects: {
-                        alley: {
-                                name: "alley",
-                                description: "You see Diagon Alley before you.",
-                        }
-                },
-                exits: {
-                        north: {
-                                direction: "north",
-                                room: "alley1",
-                        },
-                },
-        },
 
-        alley1: {
-                name: "Farther along Diagon Alley",
-                description: "Farther along Diagon Alley",
-                exits: {
-                        south: {
-                                direction: "south",
-                                room: "alleyEntrance",
-                        },
-                },
+    alleyEntrance: {
+        name: "Entrance to Diagon Alley",
+        description: "This is the entrance to Diagon Alley.",
+        on_enter: function () {
+            'use strict';
+            
+            console.log("You are now entering Diagon Alley.");
+        },
+        objects: {
+            alley: {
+                name: "alley",
+                description: "You see Diagon Alley before you."
+            }
+        },
+        exits: {
+            north: {
+                direction: "north",
+                room: "alley1"
+            }
         }
+    },
+
+    alley1: {
+        name: "Farther along Diagon Alley",
+        description: "Farther along Diagon Alley",
+        exits: {
+            south: {
+                direction: "south",
+                room: "alleyEntrance"
+            }
+        }
+    }
 };
